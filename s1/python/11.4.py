c30325bf7acc59f40b0ae32062d969b3cbb1636e
benoirajan <95347562+benoirajan@users.noreply.github.com>
2021-12-08 15:16:54 +0530

    8/12/2021

---
+++ s1/python/11.4.py
@@ -0,0 +1,16 @@
+"""
+=========================================================================
+validate password
+length min 6 max 16
+start with char or number
+should have
+  atleast
+     1 lowercase
+     1 upper
+       digit
+       symbol
+=========================================================================
+Output 
+======
+\\\\\\\\\\\\\\\\
+"""
