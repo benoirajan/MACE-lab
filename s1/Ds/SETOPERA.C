03f2881467a36965cf7438c6d52dc3d80bd3b846
BEN <becaremail@gmail.com>
2022-02-01 18:23:42 +0530

    commented output

--- s1/Ds/SETOPERA.C
+++ s1/Ds/SETOPERA.C
@@ -10,7 +10,7 @@ a[]={0,0,0,0,0,0,0,0,0,0},
 b[]={0,0,0,0,0,0,0,0,0,0};
 void main(){
   int val;
-  clrscr();
+  //clrscr();
   printf("Enter number of elements: ");
   scanf("%d",&n);
   printf("Enter elements in U\n");
@@ -53,4 +53,23 @@ void main(){
        printf("%d ",u[i]);
   getch();
 }
+
+/**
+ * @output
+Enter number of elements: 4
+Enter elements in U
+1 2 3 4
+Enter number of elements: 5
+Enter elements in U
+1 2 3 4 5
+Enter number of elements: 3
+Enter elements in A
+1 2 4
+Enter number of elements: 3
+Enter elements in B
+3 4 5
+A U B: 1 2 3 4 5
+A n B: 4
+A - B: 1 2
+ */
 
